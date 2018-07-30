859ab90e935a8a0b8d1c4f1eabd4fca154923c53
Nozhan Amiri <nozhana@gmail.com>
2018-07-30 12:01:54 +0430

    initial commit

---
+++ polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    # /polls/
+    path('', views.index, name='index'),
+
+    # /polls/question_id/
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # /polls/question_id/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # /polls/question_id/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+]
